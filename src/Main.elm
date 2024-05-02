589ce692241ca3174bd2e89e185fb16604178dbd
wgcotera <wgcotera@espol.edu.ec>
2024-05-01 21:02:42 -0500

    feat: use custom type for the grades

--- src/Main.elm
+++ src/Main.elm
@@ -22,23 +22,28 @@ main =
 
 
 type alias Model =
-    { primerParcial : Maybe Float
-    , segundoParcial : Maybe Float
-    , practico : Maybe Float
-    , mejoramiento : Maybe Float
-    , porcentajePractico : Maybe Float
-    , grade : Maybe Float
+    { primerParcial : Grade
+    , segundoParcial : Grade
+    , practico : Grade
+    , mejoramiento : Grade
+    , porcentajePractico : Grade
+    , grade : Grade
     }
+
+
+type Grade
+    = NotEntered
+    | Entered Float
 
 
 init : Model
 init =
-    { primerParcial = Nothing
-    , segundoParcial = Nothing
-    , practico = Nothing
-    , mejoramiento = Nothing
-    , porcentajePractico = Nothing
-    , grade = Nothing
+    { primerParcial = NotEntered
+    , segundoParcial = NotEntered
+    , practico = NotEntered
+    , mejoramiento = NotEntered
+    , porcentajePractico = NotEntered
+    , grade = NotEntered
     }
 
 
@@ -60,28 +65,40 @@ update : Msg -> Model -> Model
 update msg model =
     case msg of
         UpdatePrimerParcial primer_parcial ->
-            { model | primerParcial = String.toFloat primer_parcial }
+            { model | primerParcial = toGrade primer_parcial }
 
         UpdateSegundoParcial segundo_parcial ->
-            { model | segundoParcial = String.toFloat segundo_parcial }
+            { model | segundoParcial = toGrade segundo_parcial }
 
         UpdatePractico practico ->
-            { model | practico = String.toFloat practico }
+            { model | practico = toGrade practico }
 
         UpdateMejoramiento mejoramiento ->
-            { model | mejoramiento = String.toFloat mejoramiento }
+            { model | mejoramiento = toGrade mejoramiento }
 
         UpdatePorcentajePractico porcentaje_practico ->
-            { model | porcentajePractico = String.toFloat porcentaje_practico }
+            { model | porcentajePractico = toGrade porcentaje_practico }
 
         CalculateGrade ->
-            { model | grade = model.primerParcial }
+            { model
+                | grade = NotEntered
+            }
 
         CalculateGradeResult (Ok grade) ->
-            { model | grade = Just grade }
+            { model | grade = Entered grade }
 
         CalculateGradeResult (Err _) ->
-            { model | grade = Nothing }
+            { model | grade = NotEntered }
+
+
+toGrade : String -> Grade
+toGrade str =
+    case String.toFloat str of
+        Just value ->
+            Entered value
+
+        Nothing ->
+            NotEntered
 
 
 
@@ -90,19 +107,24 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [ class "h-screen flex flex-col justify-center items-center" ]
+    div [ class "h-screen flex justify-center items-center gap-6" ]
         [ div [ class "flex flex-col items-center" ]
-            [ input [ class "border border-gray-400 rounded-lg p-2 m-2", onInput UpdatePrimerParcial, placeholder "Primer Parcial" ] []
-            , input [ class "border border-gray-400 rounded-lg p-2 m-2", onInput UpdateSegundoParcial, placeholder "Segundo Parcial" ] []
-            , input [ class "border border-gray-400 rounded-lg p-2 m-2", onInput UpdatePractico, placeholder "Practico" ] []
-            , input [ class "border border-gray-400 rounded-lg p-2 m-2", onInput UpdateMejoramiento, placeholder "Mejoramiento" ] []
-            , input [ class "border border-gray-400 rounded-lg p-2 m-2", onInput UpdatePorcentajePractico, placeholder "Porcentaje Practico" ] []
+            [ inputComponent "Primer Parcial" UpdatePrimerParcial
+            , inputComponent "Segundo Parcial" UpdateSegundoParcial
+            , inputComponent "Practico" UpdatePractico
+            , inputComponent "Mejoramiento" UpdateMejoramiento
+            , inputComponent "Porcentaje Practico" UpdatePorcentajePractico
             , button [ class "border border-gray-400 rounded-lg p-2 m-2", onClick CalculateGrade ] [ text "Calcular Nota" ]
             ]
         , case model.grade of
-            Just grade ->
-                div [ class "text-2xl" ] [ text <| "Nota: " ++ String.fromFloat grade ]
+            Entered grade ->
+                div [ class "text-5xl" ] [ text <| "* " ++ String.fromFloat grade ++ " *" ]
 
-            Nothing ->
+            NotEntered ->
                 div [] []
         ]
+
+
+inputComponent : String -> (String -> msg) -> Html msg
+inputComponent placeholderMsg msg =
+    input [ class "border border-gray-400 rounded-lg p-2 m-2", onInput msg, placeholder placeholderMsg ] []
